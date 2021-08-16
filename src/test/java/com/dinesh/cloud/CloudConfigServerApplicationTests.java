0ca3283a7d0490f89fcd0948f86a9463b51d793e
dinesh <dinesh@codingmart.com>
2021-08-16 13:56:42 +0530

    first commit

---
+++ src/test/java/com/dinesh/cloud/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
